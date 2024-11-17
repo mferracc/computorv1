a2e927e0a4e9a5f2f62a209c548720a520e3d462
Marie-Ange Ferracci <opaleboreale@protonmail.com>
2024-11-18 00:10:25 +0100

    Created struct "Polynomial" used to store polynomials info

--- computorv1/src/lib.rs
+++ computorv1/src/lib.rs
@@ -1,3 +1,41 @@
 pub mod solvers;
 pub mod math_tools;
 
+pub struct Polynomial {
+    degree: u8,
+    coefficients: Vec<f64>,
+    discriminant: Some(f64),
+    solutions: Vec<f64>,
+}
+
+impl Polynomial {
+    pub fn build(args: &[String]) -> Result<Polynomial, &'static str> {
+        if args.len() < 2 {
+            return Err("wrong usage");
+        }
+
+        let degree: u8 = 0;
+        let coefficients: Vec<f64> = Vec::new();
+        let discriminant: Some(f64) = None;
+        let solutions: Vec<f64> = Vec::new();
+
+        Ok(Polynomial {
+            degree,
+            coefficients,
+            discriminant, // calc later
+            solutions, // calc later
+        })
+    }
+
+    pub fn solve() {
+        unimplemented!();
+        // calc ∆ and solutions
+    }
+
+    pub fn display_result(self) {
+        println!("Reduced form: ");
+        //println!("Polynomial degree: {}", self::degree)
+        println!("Discriminant is ...blablabla..., the two solutions are:");
+        println!("les solutions");
+    }
+}
--- computorv1/src/main.rs
+++ computorv1/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+use std::{env, process};
+use computorv1::Polynomial;
 
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let polynomial: Polynomial = Polynomial::build(&args).unwrap_or_else(|err: &str| {
+        println!("Error: {err}.");
+        process::exit(1);
+    });
+
+    // polynomial::solve;
+    // polynomial::display_result;
 }
